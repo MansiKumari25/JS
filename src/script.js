7b7aba8a919b4910d43e12eddd0556063c01073b
MansiKumari25 <mansikumari@cedcoss.com>
2022-02-05 16:04:41 +0530

    Using Objects for Lookups

--- src/script.js
+++ src/script.js
@@ -767,3 +767,21 @@ const myDog = {
   // Only change code below this line
   delete myDog.tails;
   
+
+  function phoneticLookup(val) {
+    let result = "";
+  
+    
+    const lookup={
+      "alpha": "Adams",
+      "bravo": "Boston",
+      "charlie": "Chicago",
+      "delta": "Denver",
+      "echo": "Easy",
+      "foxtrot": "Frank"
+    }
+  result=lookup[val];
+    return result;
+  }
+  
+  phoneticLookup("charlie");
