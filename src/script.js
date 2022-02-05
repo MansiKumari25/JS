0d78c6449889434ae784863d9cf62ecf37b0eb53
MansiKumari25 <mansikumari@cedcoss.com>
2022-02-05 16:17:27 +0530

    Accessing Nested Objects'

--- src/script.js
+++ src/script.js
@@ -846,3 +846,18 @@ const myDog = {
       "gold": true
     }
   ];
+
+
+  const myStorage = {
+    "car": {
+      "inside": {
+        "glove box": "maps",
+        "passenger seat": "crumbs"
+       },
+      "outside": {
+        "trunk": "jack"
+      }
+    }
+  };
+  
+  const gloveBoxContents = myStorage.car.inside["glove box"];
