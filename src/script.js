c4e0ea2d255e199727bf7720b377a70115781fe2
MansiKumari25 <mansikumari@cedcoss.com>
2022-02-05 14:45:54 +0530

    Introducing Else Statements

--- src/script.js
+++ src/script.js
@@ -447,3 +447,17 @@ function nextInLine(arr, item) {
   }
   
   testLogicalOr(15);
+
+
+  function testElse(val) {
+    let result = "";
+    if (val > 5) {
+      result = "Bigger than 5";
+    }
+    else{
+      result = "5 or Smaller";
+    }
+    return result;
+  }
+  
+  console.log(testElse(4));
