42a583d2993f2c3620da5a5218026e4c92e42053
MansiKumari25 <mansikumari@cedcoss.com>
2022-02-05 10:34:08 +0530

    DEclared a read-oonly variable

--- src/script.js
+++ src/script.js
@@ -35,3 +35,8 @@ var titleCaseOver;
 studlyCapVar = 10;
 properCamelCase = "A String";
 titleCaseOver = 9000;
+
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
