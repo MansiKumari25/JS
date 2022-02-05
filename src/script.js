9986c7768fec7687435c95b17a15af66de1a669f
MansiKumari25 <mansikumari@cedcoss.com>
2022-02-05 13:45:02 +0530

    Stand in Line

--- src/script.js
+++ src/script.js
@@ -279,3 +279,21 @@ function processArg(num) {
 }
 console.log(processed);
 
+
+
+
+function nextInLine(arr, item) {
+    
+    arr.push(item);
+    item=arr.shift();
+    return item;
+   
+  }
+  
+  
+  const testArr = [1, 2, 3, 4, 5];
+  
+  console.log("Before: " + JSON.stringify(testArr));
+  console.log(nextInLine(testArr, 6));
+  console.log("After: " + JSON.stringify(testArr));
+
