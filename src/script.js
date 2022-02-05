dcda2296067c848f8062a6b460dfb347b0f1916c
MansiKumari25 <mansikumari@cedcoss.com>
2022-02-05 15:48:53 +0530

    Accessing Object Properties with Bracket Notation'

--- src/script.js
+++ src/script.js
@@ -716,3 +716,12 @@ const myDog = {
   
   const hatValue = testObj.hat;      
   const shirtValue = testObj.shirt; 
+
+  const testObj = {
+    "an entree": "hamburger",
+    "my side": "veggies",
+    "the drink": "water"
+  };
+  
+  const entreeValue = testObj["an entree"];   
+  const drinkValue = testObj["the drink"];    
