aeadfbbce6fb84d04d824d759a44bec443345606
MansiKumari25 <mansikumari@cedcoss.com>
2022-02-05 15:57:15 +0530

    Add New Properties to a JavaScript Object

--- src/script.js
+++ src/script.js
@@ -746,3 +746,12 @@ const myDog = {
   
 
   myDog["name"]="Happy Coder";
+
+  const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+  };
+  myDog.bark="woof";
+  
