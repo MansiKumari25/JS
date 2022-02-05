8e4ba625c0cdd2884111bbaa2a4f13c1766f0aeb
MansiKumari25 <mansikumari@cedcoss.com>
2022-02-05 13:15:14 +0530

    Local Scope and Functions

--- src/script.js
+++ src/script.js
@@ -238,3 +238,12 @@ function fun2() {
   console.log(output);
 }
 
+
+function myLocalScope() {
+   let myVar;
+    console.log('inside myLocalScope', myVar);
+  }
+  myLocalScope();
+  
+  console.log('outside myLocalScope', myVar);
+
