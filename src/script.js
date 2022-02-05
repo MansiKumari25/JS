06b15fe187a26e5630f13e8a4092576ca8bf3eff
MansiKumari25 <mansikumari@cedcoss.com>
2022-02-05 14:10:53 +0530

    Comparison with the Greater Than Operator

--- src/script.js
+++ src/script.js
@@ -361,3 +361,17 @@ function nextInLine(arr, item) {
   }
   
   testStrictNotEqual(10);
+
+  function testGreaterThan(val) {
+    if (val > 100) {  
+      return "Over 100";
+    }
+  
+    if (val> 10) { 
+      return "Over 10";
+    }
+  
+    return "10 or Under";
+  }
+  
+  testGreaterThan(10);
