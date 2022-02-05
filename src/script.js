e248d425b87d47263067705ea2b5ab20d3e80256
MansiKumari25 <mansikumari@cedcoss.com>
2022-02-05 15:40:50 +0530

    Counting Cards

--- src/script.js
+++ src/script.js
@@ -659,3 +659,38 @@ function caseInSwitch(val) {
   }
   
   abTest(2,2);
+
+
+  let count = 0;
+
+function cc(card) {
+ 
+ switch(card)
+ {
+   case 2:
+   case 3:
+   case 4:
+   case 5:
+   case 6:
+    count++;
+    break;
+  case 10:
+  case 'J':
+  case 'Q':
+  case 'K':
+  case 'A':
+    count--;
+    break;  
+ }
+ if(count < 1)
+ {
+   return count+" Hold";
+ }
+ else if(count >= 1)
+ {
+   return count+" Bet";
+ }
+  
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
